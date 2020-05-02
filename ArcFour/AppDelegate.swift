9c080d7ff8e300257b1321094a4509fa6fbebb23
Purcea Florin <zapad.daniel7@gmail.com>
2020-05-02 11:30:56 +0300

    Initial Commit

---
+++ ArcFour/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArcFour
+//
+//  Created by Florin Daniel on 02/05/2020.
+//  Copyright © 2020 Florin Daniel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArcFour/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArcFour
+//
+//  Created by Florin Daniel on 02/05/2020.
+//  Copyright © 2020 Florin Daniel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
